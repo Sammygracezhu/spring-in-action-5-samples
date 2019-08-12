a574586326b98e073e497758d240b7ede89dfb1e
Xuanwei Zhu <ymibcas@gmail.com>
2019-08-12 10:46:34 -0400

    change 'ccNumber varchar(16) not null '
    change 'ccNumber varchar(16) not null ' to "ccNumber varchar(50)"
    Or get Error like 
    PreparedStatementCallback; Value too long for column """CCNUMBER"" VARCHAR(16) NOT NULL": "'1111 1111 1111 111' (18)"; SQL statement: INSERT INTO Taco_Order (DELIVERYNAME, DELIVERYSTREET, DELIVERYCITY, DELIVERYSTATE, DELIVERYZIP, CCNUMBER, CCEXPIRATION, CCCVV, PLACEDAT) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?) [22001-199]; nested exception is org.h2.jdbc.JdbcSQLDataException: Value too long for column """CCNUMBER"" VARCHAR(16) NOT NULL": "'1111 1111 1111 111' (18)"; SQL statement: INSERT INTO Taco_Order (DELIVERYNAME, DELIVERYSTREET, DELIVERYCITY, DELIVERYSTATE, DELIVERYZIP, CCNUMBER, CCEXPIRATION, CCCVV, PLACEDAT) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?) [22001-199]
    org.springframework.dao.DataIntegrityViolationException: PreparedStatementCallback; Value too long for column """CCNUMBER"" VARCHAR(16) NOT NULL": "'1111 1111 1111 111' (18)"; SQL statement:
    INSERT INTO Taco_Order (DELIVERYNAME, DELIVERYSTREET, DELIVERYCITY, DELIVERYSTATE, DELIVERYZIP, CCNUMBER, CCEXPIRATION, CCCVV, PLACEDAT) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?) [22001-199]; nested exception is org.h2.jdbc.JdbcSQLDataException: Value too long for column """CCNUMBER"" VARCHAR(16) NOT NULL": "'1111 1111 1111 111' (18)"; SQL statement:
    INSERT INTO Taco_Order (DELIVERYNAME, DELIVERYSTREET, DELIVERYCITY, DELIVERYSTATE, DELIVERYZIP, CCNUMBER, CCEXPIRATION, CCCVV, PLACEDAT) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?) [22001-199]

--- ch03/tacos-jdbc/src/main/resources/schema.sql
+++ ch03/tacos-jdbc/src/main/resources/schema.sql
@@ -27,7 +27,7 @@ create table if not exists Taco_Order (
 	deliveryCity varchar(50) not null,
 	deliveryState varchar(2) not null,
 	deliveryZip varchar(10) not null,
-	ccNumber varchar(16) not null,
+	ccNumber varchar(50) not null,
 	ccExpiration varchar(5) not null,
 	ccCVV varchar(3) not null,
     placedAt timestamp not null
